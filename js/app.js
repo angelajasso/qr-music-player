96e59d1522ee867d9473005c473e30f2ecb30455
angelajasso <angelajassoma@gmail.com>
2026-06-26 23:34:04 -0600

    feat: add pause control

--- js/app.js
+++ js/app.js
@@ -1,20 +1,23 @@
 const audioInput = document.querySelector('#audio-file');
 const fileName = document.querySelector('#file-name');
 const playButton = document.querySelector('#play-button');
+const pauseButton = document.querySelector('#pause-button');
 
 const audio = new Audio();
 
 audioInput.addEventListener('change', () => {
-
     const file = audioInput.files[0];
 
     if (file) {
         fileName.textContent = file.name;
         audio.src = URL.createObjectURL(file);
     }
-
 });
 
 playButton.addEventListener('click', () => {
     audio.play();
+});
+
+pauseButton.addEventListener('click', () => {
+    audio.pause();
 });
