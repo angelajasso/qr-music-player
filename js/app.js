4d786d472c0aeb22bcb2a948c65aae552ac2f31b
angelajasso <angelajassoma@gmail.com>
2026-06-28 23:39:38 -0600

    feat: add audio progress bar

--- js/app.js
+++ js/app.js
@@ -1,40 +1,90 @@
+// Inputs
 const audioInput = document.querySelector('#audio-file');
+
+// Information
 const fileName = document.querySelector('#file-name');
-const duration = document.querySelector('#audio-duration');
+const currentTime = document.querySelector('#current-time');
+const totalTime = document.querySelector('#total-time');
+
+// Controls
 const playButton = document.querySelector('#play-button');
 const pauseButton = document.querySelector('#pause-button');
+const progressBar = document.querySelector('#progress-bar');
 
+// Audio Player
 const audio = new Audio();
+
+
+// ==========================
+// Select Audio File
+// ==========================
 
 audioInput.addEventListener('change', () => {
 
     const file = audioInput.files[0];
 
-    if (file) {
+    if (!file) return;
 
-        fileName.textContent = file.name;
+    fileName.textContent = file.name;
 
-        audio.src = URL.createObjectURL(file);
-
-        audio.addEventListener('loadedmetadata', () => {
-
-            const minutes = Math.floor(audio.duration / 60);
-            const seconds = Math.floor(audio.duration % 60);
-
-            const formattedSeconds = String(seconds).padStart(2, '0');
-
-            duration.textContent = `Duration: ${minutes}:${formattedSeconds}`;
-
-        });
-
-    }
+    audio.src = URL.createObjectURL(file);
 
 });
 
+
+// ==========================
+// Audio Metadata
+// ==========================
+
+audio.addEventListener('loadedmetadata', () => {
+
+    const minutes = Math.floor(audio.duration / 60);
+    const seconds = Math.floor(audio.duration % 60);
+
+    const formattedSeconds = String(seconds).padStart(2, '0');
+
+    totalTime.textContent = `${minutes}:${formattedSeconds}`;
+
+});
+
+
+// ==========================
+// Audio Progress
+// ==========================
+
+audio.addEventListener('timeupdate', () => {
+
+    // Current Time
+
+    const minutes = Math.floor(audio.currentTime / 60);
+    const seconds = Math.floor(audio.currentTime % 60);
+
+    const formattedSeconds = String(seconds).padStart(2, '0');
+
+    currentTime.textContent = `${minutes}:${formattedSeconds}`;
+
+    // Progress Bar
+
+    const progress =
+        (audio.currentTime / audio.duration) * 100;
+
+    progressBar.value = progress;
+
+});
+
+
+// ==========================
+// Controls
+// ==========================
+
 playButton.addEventListener('click', () => {
+
     audio.play();
+
 });
 
 pauseButton.addEventListener('click', () => {
+
     audio.pause();
+
 });
